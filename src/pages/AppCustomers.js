a0be2fca1f965234ac3a09e993c9d29d1c95681f
dusanPapovic <papovicdusan91@gmail.com>
2021-11-12 19:38:52 +0100

    3 zadatak

--- src/pages/AppCustomers.js
+++ src/pages/AppCustomers.js
@@ -20,29 +20,75 @@ function AppCustomers() {
     },
   ]);
 
+  const [newCustomer, setNewCustomer] = useState({
+    name: "",
+    surname: "",
+  });
+
   const handleDelete = (deleteCustomerId) => {
     setCustomers(customers.filter((el) => el.id !== deleteCustomerId));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setCustomers([...customers, { ...newCustomer }]);
+
+    setNewCustomer({
+      name: "",
+      surname: "",
+    });
+  };
+
   return (
     <div>
-      {customers.map((customer) => (
-        <div className="container" key={customer.id}>
-          <div className="row pt-3">
-            <div className="col-2">
-              {customer.name} {customer.surname}
-            </div>
-            <div className="col-3">
-              <button
-                className="btn btn-primary"
-                onClick={() => handleDelete(customer.id)}
-              >
-                Delete
-              </button>
+      <div>
+        {customers.map((customer) => (
+          <div className="container" key={customer.id}>
+            <div className="row pt-3">
+              <div className="col-2">
+                {customer.name} {customer.surname}
+              </div>
+              <div className="col-3">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleDelete(customer.id)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
           </div>
+        ))}
+      </div>
+
+      <div className="container">
+        <div className="row pt-3">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                value={newCustomer.name}
+                placeholder="Ime"
+                onChange={(e) =>
+                  setNewCustomer({ ...newCustomer, name: e.target.value })
+                }
+              />
+            </div>
+            <div className="form-group">
+              <input
+                value={newCustomer.surname}
+                placeholder="Prezime"
+                onChange={(e) =>
+                  setNewCustomer({ ...newCustomer, surname: e.target.value })
+                }
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary">Dodaj kupca</button>
+            </div>
+          </form>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
